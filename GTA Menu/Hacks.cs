c7e3a1daef677a607bd4e223d0c63217176ca664
EdgyCoder ✌ <administrator@kawaaii.moe>
2021-01-14 21:24:20 +0100

    first commit

---
+++ GTA Menu/Hacks.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace GTA_Menu {
+    class Hacks {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);
+
+        public enum ThreadAccess : int {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002)
+        }
+
+        public static void SuspendTask(Process process) {
+            foreach (ProcessThread thread in process.Threads) {
+                IntPtr openThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+                if (openThread == IntPtr.Zero) continue;
+
+                SuspendThread(openThread);
+                CloseHandle(openThread);
+            }
+
+            System.Threading.Thread.Sleep(9000);
+
+            foreach (ProcessThread thread in process.Threads) {
+                IntPtr openThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)thread.Id);
+                if (openThread == IntPtr.Zero) continue;
+
+                var suspendCount = 0;
+                do {
+                    suspendCount = ResumeThread(openThread);
+                } while (suspendCount > 0);
+
+                CloseHandle(openThread);
+            }
+        }
+
+        public static void ForceEndTask(Process process) {
+            process.Kill();
+        }
+    }
+}
---
+++ GTA Menu/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+using static GTA_Menu.HotKeyManager;
+
+namespace GTA_Menu {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GTAOnlineSuspender());
+        }
+    }
+
+    public class GTAOnlineSuspender : ApplicationContext {
+        private NotifyIcon trayIcon;
+
+        public GTAOnlineSuspender() {
+            trayIcon = new NotifyIcon();
+
+            RegisterKey(Keys.S, KeyModifiers.Alt | KeyModifiers.Control);
+            RegisterKey(Keys.X, KeyModifiers.Alt | KeyModifiers.Control);
+            HotKeyPressed += new EventHandler<HotKeyEventArgs>(OnHotKeyPressed);
+
+            ContextMenu contextMenu = new ContextMenu();
+            contextMenu.MenuItems.Add(new MenuItem("Suspend GTA", (object sender, EventArgs e) => SuspendTask(), (Shortcut)(Keys.Control | Keys.Alt | Keys.S)));
+            contextMenu.MenuItems.Add(new MenuItem("Force Quit GTA", (object sender, EventArgs e) => ForceEndTask(), (Shortcut)(Keys.Control | Keys.Alt | Keys.X)));
+            contextMenu.MenuItems.Add("-");
+            contextMenu.MenuItems.Add(new MenuItem("Exit", (object sender, EventArgs e) => { trayIcon.Visible = false; Application.Exit(); }));
+
+            trayIcon.Icon = Properties.Resources.SuspendIcon;
+            trayIcon.ContextMenu = contextMenu;
+            trayIcon.Visible = true;
+        }
+
+        void OnHotKeyPressed(object sender, HotKeyEventArgs args) {
+            switch (args.Key) {
+                case Keys.X: ForceEndTask(); break;
+                case Keys.S: SuspendTask(); break;
+                default: break;
+            }
+        }
+
+        public void ForceEndTask() {
+            Process[] processes = Process.GetProcessesByName("GTA5");
+            if (processes.Length == 0) return;
+            Hacks.ForceEndTask(processes[0]);
+        }
+
+        public void SuspendTask() {
+            Process[] processes = Process.GetProcessesByName("GTA5");
+            if (processes.Length == 0) return;
+            Hacks.SuspendTask(processes[0]);
+        }
+    }
+}
